9eff7804fc0f0e949ac548a692a7cd11b5b002f8
AJJ0nes <0869525@my.scccd.edu>
2025-08-18 12:11:44 -0700

    Add files via upload

---
+++ addTwoInts.cpp
@@ -0,0 +1,36 @@
+// addTwoints
+// AJJ 08/18/25
+// Refrence
+//https://www.w3schools.com/c/c_user_input.php
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to my largest of three ints program. \n";
+
+    //create variables
+    int num1;
+    int num2;
+    int num3;
+    int sumofinputs = 0;
+//collect varaibles from user
+    cout<< "\n Please enter vaule.";
+    cin >> num1;
+    cout<< "\n Please enter vaule.";
+    cin >> num2;
+    cout<< "\n Please enter vaule.";
+    cin >> num3;
+//compare varaibles for highest value
+    if (num1 > num2) {
+        if (num1 > num3) {
+            cout << num1;
+        }else cout << num3;
+
+    }else if (num2 > num3) {
+        cout << num2;
+    }else cout << num3;
+    return 0;
+}
